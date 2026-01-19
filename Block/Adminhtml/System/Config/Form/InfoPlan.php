6757bbe7738a149d2c8aee779ab89f0e77bd6f8f
ivan.hrytsai <ivan.hrytsai@magefan.com>
2026-01-19 09:23:03 +0200

    14604-Upgrade-plan-popup-UTM

--- Block/Adminhtml/System/Config/Form/InfoPlan.php
+++ Block/Adminhtml/System/Config/Form/InfoPlan.php
@@ -83,7 +83,7 @@ abstract class InfoPlan extends \Magefan\Community\Block\Adminhtml\System\Config
                                             text: "Upgrade Plan Now",
                                             class: "action primary accept",
                                             click: function () {
-                                                window.open("https://magefan.com/magento-2-extensions");
+                                                window.open("https://magefan.com/magento-2-security-extension/pricing?utm_source=admin&utm_medium=config&utm_campaign=upgrade-popup");
                                             }
                                         }]
                                     });
